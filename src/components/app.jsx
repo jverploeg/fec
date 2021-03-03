f5753233401e6a339824eb91717a2fe934b2e2bf
Jack <verploegjack@gmail.com>
2021-03-03 13:50:06 -0700

    trying to get button working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => (
-  <div className="main">
-    <div className="overview">
-      ProductOverview
+function App() {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+    // eslint-disable-next-line no-console
+    console.log('clicked');
+  };
+  return (
+    <div className="main">
+      <div>
+        <p>{count}</p>
+        <button type="button" onClick={() => console.log('click')}>
+          Click me
+        </button>
+      </div>
+      <div className="overview">
+        ProductOverview
+      </div>
+      <div className="related">
+        RelatedProducts
+      </div>
+      <div className="ratings">
+        RatingsReviews
+      </div>
     </div>
-    <div className="related">
-      RelatedProducts
-    </div>
-    <div className="ratings">
-      RatingsReviews
-    </div>
-  </div>
-);
+  );
+}
 
 export default App;
