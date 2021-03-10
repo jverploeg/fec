70f1cb97ddc574aa444b1f2b447305facb6a59aa
Joseph Monreal <joemonreal23@icloud.com>
2021-03-10 14:54:17 -0700

    Related Products - Added Modal - Need to add actual data for component.

--- src/components/products/Images.jsx
+++ src/components/products/Images.jsx
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { MdStar } from 'react-icons/md';
 
 const Images = (props) => {
   useEffect(() => {
     const allGliders = document.querySelectorAll('.glider-images');
     allGliders.forEach(gliderSet => {
       new Glider(gliderSet, {
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true
       });
     });
   });
+
+  function handleClick(e) {
+    console.log(e);
+    if ( e.target.style.color === '' ) {
+      e.target.style.color = 'yellow';
+      e.target.style.className = 'modal-close is-large';
+      e.target.style.ariaLabel = 'close';
+    } else {
+      e.target.style.color = '';
+    }
+  }
+
   return (
-    <div className="glider-images">
-    { props.theImages.map((img, idx) => (
-      <div key={img + "" + idx} className="card-image">
-        <figure className="image is-4by3 is-fullwidth">
-          <img src={img} alt="" />
-        </figure>
+    <div>
+      <div className="glider-images">
+      { props.theImages.map((img, idx) => (
+        <div key={img + "" + idx} className="card-image" style={{position: 'relative'}}>
+          <figure className="image is-4by3">
+            <img src={img} alt="" />
+            <MdStar onClick={handleClick} size={30} color='white' style={{position: 'absolute', top: '0%', right: '0%'}}/>
+          </figure>
+        </div>
+      ))}
       </div>
-    ))}
     </div>
   )
 };
