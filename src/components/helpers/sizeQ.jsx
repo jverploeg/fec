97cb44a82bdb72c5d57e007970a01c24b11d772c
Jack <verploegjack@gmail.com>
2021-03-11 19:08:04 -0700

    size and quantity selection working. clean up and add cart functionality

--- src/components/helpers/sizeQ.jsx
+++ src/components/helpers/sizeQ.jsx
@@ -4,26 +4,61 @@ import React, { useState, useEffect } from 'react';
 const DropDown = (props) => {
   // props will be a set of {quantity: num, size: xs-xl}
   const [state, setState] = useState('');
-  const [size, setSizes] = useState('-');
+  const [sizes, setSizes] = useState([]);
+  const [currentSize, setCurrentSize] = useState('-');
   const { style } = props;
-  console.log({style});
+  // console.log({style});
   const { skus } = style;
-  console.log({skus});
+  // console.log({skus});
   let options = [];
   if (skus) { options = Object.values(skus); }
 
-  let showValue = '';
   const maxLength = 15;
 
+  // const changeSize = () => {
+  //   options.forEach((item) => {
+  //     console.log(item.size);
+  //     console.log({state});
+  //     if (item.size === state) { console.log(item.quantity); }
+  //   });
+  // };
+  // console.log({state})
   // useEffect(() => {
-  //   setState()
-  // })
+  //   setState(state);
+  // }, [set]);
 
-
+  // useEffect(() => {
+  //   let newSize = changeSize();
+  //   console.log({newSize});
+  // }, [state]);
+  // const handleChange= (event) => {
+  //   changePhoto(event);
+  //   useEffect(() => {
+  //     setCurrentStyle(event);
+  //   });
+  // };
   const set = (event) => {
-    console.log(event);
-    showValue = event;
+    setState(event);
+    setCurrentSize(1);
   };
+  const setSize = (event) => {
+    setCurrentSize(event);
+  };
+
+  useEffect(() => {
+    let newSize = 0;
+    options.forEach((item) => {
+      if (item.size === state) { newSize = (item.quantity); }
+    });
+    //console.log(newSize);
+    let arr = [...Array(newSize).keys()];
+    arr.shift();
+    setSizes(arr);
+  }, [state]);
+  // console.log([...Array(10).keys()]);
+  // console.log({options2});
+  // console.log({sizes});
+
   // const [size, SizeDropdown] = DropDown('Select Size', '', '', sizes);
   // (name, label, defaultState, options)
   // const [state, setState] = useState(defaultState);
@@ -33,26 +68,27 @@ const DropDown = (props) => {
         <select
           id="size"
           value={state}
-          onChange={(e) => setState(e.target.value)}
-          // onChange={(e) => set(e.target.value)}
-          // disabled={!sets.length}
-        >
-          <option>Select Size</option>
-          {!!options && options.map((item) =>
-            <option key={item.size} value={item.size}>{item.size}</option>)}
-        </select>
-      </div>
-      <div className="column quantity">
-        <select
-          id=""
-          value={showValue}
+          // onChange={(e) => setState(e.target.value)}
           onChange={(e) => set(e.target.value)}
           // disabled={!sets.length}
         >
           <option>Select Size</option>
           {!!options && options.map((item) =>
-            <option key={item.size} value={item.size}>{item.size}</option>)}
-          </select>
+            <option key={item.id} value={item.size}>{item.size}</option>)}
+        </select>
+      </div>
+      <div className="column quantity">
+        <select
+          id="quantity"
+          // value={(sizes === 0) ? '-' : {currentSize} }
+          value={currentSize}
+          onChange={(e) => setSize(e.target.value)}
+          // disabled={!sets.length}
+        >
+          <option> - </option>
+          {sizes.map((index) =>
+            <option key={index.toString()} value={index}>{index}</option>)}
+        </select>
       </div>
     </div>
   );
